9b43c33f293ec225f45756caf3a6cf8b575c165c
Resdan Logan <rezn5447@gmail.com>
2018-03-03 15:41:37 -0800

    stack nav included too

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
-import { AuthScreen, WelcomeScreen } from './src/screens';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import {
+	AuthScreen,
+	WelcomeScreen,
+	MapScreen,
+	DeckScreen,
+	SettingsScreen,
+	ReviewScreen
+} from './src/screens';
 
 export default class App extends Component {
 	render() {
 		const MainNavigator = TabNavigator(
 			{
 				welcome: { screen: WelcomeScreen },
-				auth: { screen: AuthScreen }
+				auth: { screen: AuthScreen },
+				main: {
+					screen: TabNavigator({
+						map: { screen: MapScreen },
+						deck: { screen: DeckScreen },
+						review: {
+							screen: StackNavigator({
+								review: { screen: ReviewScreen },
+								settings: { screen: SettingsScreen }
+							})
+						}
+					})
+				}
 			},
 			{
 				tabBarPosition: 'bottom'
