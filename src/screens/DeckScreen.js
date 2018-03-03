ec68b5527a5e775acb4e10e91070297692c12f45
Resdan Logan <rezn5447@gmail.com>
2018-03-03 14:30:22 -0800

    screen boilerplate

---
+++ src/screens/DeckScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class DeckScreen extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>I'm the DeckScreen component</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
---
+++ src/screens/MapScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class MapScreen extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>I'm the MapScreen component</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
