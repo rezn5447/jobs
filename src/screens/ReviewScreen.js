56960db2fd76874a5393ae59b2fa85bf459ade58
Resdan Logan <rezn5447@gmail.com>
2018-03-03 18:08:26 -0800

    create reviewScreen headerStyle

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends Component {
 	static navigationOptions = ({ navigation }) => {
 		return {
-			title: 'Review Jobs',
+			title: Platform.OS === 'android' ? 'Review Jobs' : 'Bloop',
+			headerStyle: {
+				marginTop: Platform.OS === 'android' ? 10 : 0
+			},
 			headerRight: (
 				<Button
 					title="Settings"
 					onPress={() => {
 						navigation.navigate('settings');
 					}}
+					backgroundColor="rgba(0,0,0,0)"
+					color="rgba(0,122,255,1)"
 				/>
 			)
 		};
