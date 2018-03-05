973a9ec3d26b11722ea268fc92f5d5a0f86ccf26
Resdan Logan <rezn5447@gmail.com>
2018-03-05 09:55:33 -0800

    colors added to slide data

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -3,8 +3,9 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Slides } from '../components';
 
 const SLIDE_DATA = [
-	{ text: 'Welcome to JobApp' },
-	{ text: 'Set your Location, then swipe away' }
+	{ text: 'Welcome to JobApp', color: '#03A8F4' },
+	{ text: 'Use this to get a job', color: '#009688' },
+	{ text: 'Set your Location, then swipe away', color: '#03A8F4' }
 ];
 
 export default class WelcomeScreen extends Component {
