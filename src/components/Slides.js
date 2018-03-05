1c2bf676628bd55c208728f586a90feced951a51
Resdan Logan <rezn5447@gmail.com>
2018-03-05 09:31:56 -0800

    create slides component and exports

---
+++ src/components/Slides.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Slides extends Component {
+	render() {
+		return (
+			<View>
+				<Text>I'm the Slides component</Text>
+			</View>
+		);
+	}
+}
