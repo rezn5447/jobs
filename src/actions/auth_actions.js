2d1f77d32cbf39c319ba2b93c7785e01b8aed97d
Resdan Logan <rezn5447@gmail.com>
2018-03-05 16:12:02 -0800

    creating auth_action async functions

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,3 +1,11 @@
+import { AsyncStorage } from 'react-native';
+
 import { FACEBOOK_LOGIN_SUCCESS } from './types';
 
-export const facebookLogin = () => {};
+//AsyncStorage.setItem('fb_token', token);
+
+//AsyncStorage.getItem('fb_token')
+
+export const facebookLogin = () => {
+	AsyncStorage.getItem('fb_token');
+};
