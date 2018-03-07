baf368c29f84751b9ae9c68a0fb1f3c3f5564910
Resdan Logan <rezn5447@gmail.com>
2018-03-06 18:11:53 -0800

    add latlng-to-zip

--- src/actions/job_actions.js
+++ src/actions/job_actions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
 
 import { FETCH_JOBS } from './types';
 
-export const fetchJobs = () => {};
+export const fetchJobs = region => async dispatch => {
+	let { response } = axios.get(
+		`http://api.indeed.com/ads/apisearch?publisher=4201738803816157&q=java&l=austin&v=2&format=json`
+	);
+};
