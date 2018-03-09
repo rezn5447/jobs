9fdde9b3961656adbdab1f5b2a77edcb2445d269
Resdan Logan <rezn5447@gmail.com>
2018-03-08 17:28:45 -0800

    include redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { AsyncStorage } from 'react-native';
 import reducers from '../reducers';
 
 const store = createStore(
 	reducers,
 	{},
-	compose(applyMiddleware(thunk, logger))
+	compose(applyMiddleware(thunk, logger), autoRehydrate())
 );
+
+persistStore(store, { storage: AsyncStorage, whitelist: ['likes'] });
+
 export default store;
